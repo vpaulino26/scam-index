20ecb992fbaef3ecd133058c63ce7574f7897d3e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-12-24 05:39:26 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.scamindex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/bs_detail_image_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'bs_detail_image_widget.dart' show BsDetailImageWidget;
+import 'package:flutter/material.dart';
+
+class BsDetailImageModel extends FlutterFlowModel<BsDetailImageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
